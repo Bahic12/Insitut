5d7fc1beccab05bfebf0f34c9dadeaa63630f3d7
Bahic12 <89450490+Bahic12@users.noreply.github.com>
2021-08-24 07:16:14 -0700

    Update work_2_2.py

--- work2/work_2_2.py
+++ work2/work_2_2.py
@@ -17,9 +17,25 @@ class CarSalon:
     def __setitem__(self,index,value):
         self.cars[index] = value
     
+    def __call__(self,*value):
+        if value:
+            for car in value:
+               self.add_car(car)
+        else:
+            return [car for car in self.cars]
+        
+    def __add__(self,y):
+        if isinstance(y, CarSalon()):
+            new_salon = CarSalon(f"{self.name} {y.name}")
+            new_salon.cars = self.cars + y.cars
+            return new_salon
+        elif isinstance(y,Car):
+            self.add_car(y)
+    
     def add_car(self,*value):
         for car in value:
             if isinstance(car,Car):
                 self.cars.append(car)
             else:
                 print("Enter the machine")
+                
